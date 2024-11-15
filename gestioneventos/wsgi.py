b634d866d96feb91025935f4acf2085cfe635859
JleivSuaxy <jleivsuaxy@gmail.com>
2024-11-15 15:47:48 -0500

    Better configuration

--- gestioneventos/wsgi.py
+++ gestioneventos/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestioneventos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestioneventos.settings.local')
 
 application = get_wsgi_application()
