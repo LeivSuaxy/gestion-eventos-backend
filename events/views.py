41eaa80be92216899c366f6957d8da4ac6e990e2
JleivSuaxy <jleivsuaxy@gmail.com>
2024-11-17 15:01:48 -0500

    Organizer view added.

--- events/views.py
+++ events/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from common.strategy.authpermission import IsOrganizer
+from common.services.service import EventService
 
 # Create your views here.
+class OrganizerEventView(APIView):
+    # permission_classes = [IsOrganizer]
+
+    @staticmethod
+    def get(request):
+        return EventService.get()
+
+    @staticmethod
+    def post(request):
+        return EventService.post(request.data)
+
+    @staticmethod
+    def put(request):
+        return EventService.put(request.data)
+
+    @staticmethod
+    def delete(request):
+        return EventService.delete(request.data)
