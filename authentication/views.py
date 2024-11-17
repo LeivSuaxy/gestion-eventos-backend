75bb5cdafd74a4c1acf6b78996c2a976d6bf0a48
JleivSuaxy <jleivsuaxy@gmail.com>
2024-11-17 08:57:10 -0500

    Added event templates.

--- authentication/views.py
+++ authentication/views.py
@@ -1,2 +1,23 @@
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+
 # Create your views here.
 
+class Login(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        login_serializer = self.serializer_class(data=request.data, context={'request': request})
+
+        if login_serializer.is_valid():
+            user = login_serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            if created:
+                print(token)
+                print(token.key)
+                return Response({
+                    'token': token.key
+                }, status = status.HTTP_201_CREATED)
+
+        return Response(login_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- events/models.py
+++ events/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Event (models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField()
+    price = models.FloatField()
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.title} with price {self.price}'
+
+class EventParticipant (models.Model):
+    id = models.AutoField(primary_key=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} is going to {self.event.title}'
