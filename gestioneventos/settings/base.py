6b242423ec6f66747606ed9c0dc4e06edeb4be12
jleivsuaxy <jleivsuaxy@gmail.com>
2024-12-23 23:32:33 -0500

    Config celery.

--- gestioneventos/settings/base.py
+++ gestioneventos/settings/base.py
@@ -139,3 +139,11 @@ SIMPLE_JWT = {
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
 }
+
+# Celery
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
