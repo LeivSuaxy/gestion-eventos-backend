60d907f04044a4793e1d96c5a3807e1f0859018c
jleivsuaxy <jleivsuaxy@gmail.com>
2024-12-24 10:00:27 -0500

    Fixed Auth id constraint error.

--- authentication/models.py
+++ authentication/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from authentication.enum.roles import Role
+from uuid import uuid4
 
 # Create your models here.
 class EventUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
+    email = models.EmailField(unique=True)
     role = models.CharField(max_length=20, choices=[(role.value, role.name) for role in Role], default=Role.USER.value)
