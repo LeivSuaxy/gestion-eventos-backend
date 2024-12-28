a685179f52e3cf75172489975435146f4168e7ae
jleivsuaxy <jleivsuaxy@gmail.com>
2024-12-28 16:42:54 -0500

    Better mail service.

--- authentication/services/mails.py
+++ authentication/services/mails.py
@@ -1,11 +1,20 @@
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 from django.core.cache import cache
 from django.conf import settings
 
 def send_verification_email(user, code):
     subject = 'Your Verification Code'
-    message = f'Your verification code is {code}'
     from_email = settings.DEFAULT_FROM_EMAIL
     recipient_list = [user.email]
-    send_mail(subject, message, from_email, recipient_list)
+
+    # Render the HTML template with the verification code
+    html_content = render_to_string('mails/eventinvitation.html', {'verification_code': code})
+
+    # Create the email
+    email = EmailMultiAlternatives(subject, '', from_email, recipient_list)
+    email.attach_alternative(html_content, "text/html")
+    email.send()
+
+    # Cache the verification code
     cache.set(user.email, code, timeout=120)
