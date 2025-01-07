11f6dd4614e7e7ebb92d05bd401e8e37e67e463d
jleivsuaxy <jleivsuaxy@gmail.com>
2025-01-06 20:53:22 -0500

    Permissions.

--- administration/api/views.py
+++ administration/api/views.py
@@ -1,50 +1,16 @@
-from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from core.guards.permission_classes import IsAdmin
+from common.abstract.apiviews import BaseAdminApiView
 from events.services.eventservice import EventService
-from rest_framework.response import Response
 from administration.api.serializer import EventSerializerAdmin
-from rest_framework import status
 
 # Create your views here.
-class EventAdminAPIVIEW(APIView):
+class EventAdminAPIVIEW(BaseAdminApiView):
     permission_classes = [IsAdmin]
     service = EventService()
     parser_classes = (MultiPartParser, FormParser)
+    cache_key = 'events'
+    object_name_many = 'events'
+    object_name_single = 'event'
+    serializer_class = EventSerializerAdmin
 
-    def get(self, request):
-        events = self.service.get_all()
-
-        if not events:
-            return Response({'message': "There are not events in database"} ,status=status.HTTP_404_NOT_FOUND)
-
-        serializer = EventSerializerAdmin(events, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = EventSerializerAdmin(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request):
-        event = self.service.get_by_id(request.data['id'])
-
-        if event is None:
-            return Response({'message': 'Event not found'}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = EventSerializerAdmin(event, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request):
-        event = self.service.get_by_id(request.data['id'])
-
-        if event is None:
-            return Response({'message': 'Event not found'}, status=status.HTTP_404_NOT_FOUND)
-
-        event.delete()
-        return Response({'message': 'Event deleted'}, status=status.HTTP_200_OK)
